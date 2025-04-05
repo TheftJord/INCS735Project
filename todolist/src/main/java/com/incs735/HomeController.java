9c1c8507b3006b3c56acbb2cfe31946d3dda4d72
TheftJord <voltstar78@gmail.com>
2025-04-05 15:12:20 -0400

    minor changes to the base
    just pre-changing some things that might cause issues if we don't change them now

--- todolist/src/main/java/com/incs735/HomeController.java
+++ todolist/src/main/java/com/incs735/HomeController.java
@@ -3,10 +3,10 @@ package com.incs735;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class HomeController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        App.setRoot("");
     }
 }
