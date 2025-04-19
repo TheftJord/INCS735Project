74876b33c9c70e6bf83264a55dbc8ce9fb4957f5
TheftJord <voltstar78@gmail.com>
2025-04-19 17:41:20 -0400

    fixed gson
    FUCK

--- todolist/src/main/java/module-info.java
+++ todolist/src/main/java/module-info.java
@@ -3,8 +3,9 @@ module com.incs735 {
     requires javafx.fxml;
     requires java.desktop;
     requires java.logging;
+    requires java.sql;
     requires com.google.gson;
 
-    opens com.incs735 to javafx.fxml,com.google.gson;
+    opens com.incs735 to javafx.fxml, com.google.gson;
     exports com.incs735;
 }
